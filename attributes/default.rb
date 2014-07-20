ed678c3cc4f29e276a8209d21e56ed6814ba32c8
d9m <kumagai@sencorp.co.jp>
2014-07-21 01:26:43 +0900

    typo

--- attributes/default.rb
+++ attributes/default.rb
@@ -3,5 +3,5 @@
 # Attributes:: default
 #
 default["pleiades"]["url"] = "http://jaist.dl.sourceforge.jp/mergedoc/61474/pleiades_1.5.0.zip"
-default["pleiade"]["darkjuno"] = "https://s3-ap-northeast-1.amazonaws.com/gui.development.environment/Eclipse-Juno-Dark.zip"
+default["pleiades"]["darkjuno"] = "https://s3-ap-northeast-1.amazonaws.com/gui.development.environment/Eclipse-Juno-Dark.zip"
 
