153c2734e500045c9ca190e81f9bb4cb286b41a7
d9m <kumagai@sencorp.co.jp>
2014-07-21 01:04:55 +0900

    add attribute

---
+++ attributes/default.rb
@@ -0,0 +1,7 @@
+#
+# Cookbook Name:: chef-pleiades
+# Attributes:: default
+#
+default["pleiades"]["url"] = "http://jaist.dl.sourceforge.jp/mergedoc/61474/pleiades_1.5.0.zip"
+default["pleiade"]["darkjuno"] = "https://s3-ap-northeast-1.amazonaws.com/gui.development.environment/Eclipse-Juno-Dark.zip"
+
