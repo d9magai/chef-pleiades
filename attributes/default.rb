1d2d426b0156192dfd9b1f16201a62ffbc4a1284
d9magai <kumagai@sencorp.co.jp>
2015-02-20 22:08:41 +0900

    modify pleiades url

--- attributes/default.rb
+++ attributes/default.rb
@@ -2,5 +2,5 @@
 # Cookbook Name:: chef-pleiades
 # Attributes:: default
 #
-default["pleiades"]["url"] = "http://jaist.dl.sourceforge.jp/mergedoc/61474/pleiades_1.5.0.zip"
+default["pleiades"]["url"] = "http://svn.sourceforge.jp/svnroot/mergedoc/trunk/Pleiades/build/pleiades.zip"
 
