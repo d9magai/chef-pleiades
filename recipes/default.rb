0f15481c12339db6e84f534be0a90eeefbf58b2a
d9magai <kumagai@sencorp.co.jp>
2015-06-29 15:49:25 +0900

    refactor

--- recipes/default.rb
+++ recipes/default.rb
@@ -1,23 +1,20 @@
 include_recipe 'eclipse'
 
 eclipse_dir = "#{node['ark']['prefix_home']}/eclipse"
-pleiades_dir = '/tmp/pleiades'
-pleiades_zip = pleiades_dir + '/' + (File.basename node['pleiades']['url'])
+pleiades_filename=File.basename node['pleiades']['url']
 
-directory pleiades_dir do
-	action :create
-	not_if { ::File.exists?(pleiades_dir) }
+remote_file "#{Chef::Config[:file_cache_path]}/#{pleiades_filename}" do
+  source node['pleiades']['url']
 end
 
 execute 'install_pleiades' do
-	cwd pleiades_dir
-	command <<-EOH
-	wget #{node['pleiades']['url']}
-	unzip #{pleiades_zip} -d ./
-	cp -f -r features/jp.sourceforge.mergedoc.pleiades #{eclipse_dir}/features
-	cp -f -r plugins/jp.sourceforge.mergedoc.pleiades #{eclipse_dir}/plugins
-	EOH
-	not_if { ::File.exists?("#{pleiades_dir}/plugins/jp.sourceforge.mergedoc.pleiades")}
+        cwd Chef::Config[:file_cache_path]
+        command <<-EOH
+        unzip "#{Chef::Config[:file_cache_path]}/#{pleiades_filename}" -d ./
+        cp -f -r features/jp.sourceforge.mergedoc.pleiades #{eclipse_dir}/features
+        cp -f -r plugins/jp.sourceforge.mergedoc.pleiades #{eclipse_dir}/plugins
+        EOH
+        not_if { ::File.exists?("#{Chef::Config[:file_cache_path]}/plugins/jp.sourceforge.mergedoc.pleiades")}
 end
 
 template "#{eclipse_dir}/eclipse.ini" do
